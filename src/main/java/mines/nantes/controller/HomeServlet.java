9135602ea820f68b87b9bd223de10c92197c342d
Maxime <maxime.claveau@etudiant.mines-nantes.fr>
2015-10-22 16:40:20 +0200

    Ihm Début

--- src/main/java/mines/nantes/controller/HomeServlet.java
+++ src/main/java/mines/nantes/controller/HomeServlet.java
@@ -3,6 +3,7 @@ package mines.nantes.controller;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.servlet.RequestDispatcher;
 import java.io.IOException;
 
 /**
@@ -15,10 +16,29 @@ public class HomeServlet extends javax.servlet.http.HttpServlet {
     }
 
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("h2unit");
-        //EntityManager em = emf.createEntityManager();
-        //em.getTransaction().commit();
-        emf.close();
+        String forward = request.getPathInfo();
+        RequestDispatcher dispatcher;
 
+        switch(forward){
+            case "/reservation":
+                request.setAttribute("page","reservation");
+                break;
+            case "/typeRessource":
+                request.setAttribute("page","typeRessource");
+                break;
+            case "/ressource":
+                request.setAttribute("page","ressource");
+                break;
+            case "/utilisateur":
+                request.setAttribute("page","utilisateur");
+                break;
+            case "/reservationAdmin":
+                request.setAttribute("page","reservationAdmin");
+                break;
+            default:
+                request.setAttribute("page","reservation");
+        }
+        dispatcher=request.getRequestDispatcher("/html/template.jsp");
+        dispatcher.forward(request,response);
     }
 }
---
+++ src/main/java/mines/nantes/controller/LoginFilter.java
@@ -0,0 +1,36 @@
+package mines.nantes.controller;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public abstract class LoginFilter implements javax.servlet.Filter {
+    protected ServletContext servletContext;
+
+    public void init(FilterConfig filterConfig) {
+        servletContext = filterConfig.getServletContext();
+    }
+
+    public void doFilter(
+            ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest)request;
+        HttpServletResponse resp = (HttpServletResponse)response;
+
+        if (!isAuth()) {
+            resp.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+            return; //break filter chain, requested JSP/servlet will not be executed
+        }
+
+        //propagate to next element in the filter chain, ultimately JSP/ servlet gets executed
+        chain.doFilter(request, response);
+    }
+
+    /**
+     * logic to accept or reject access to the page, check log in status
+     * @return true when authentication is deemed valid
+     */
+    protected abstract boolean isAuth();
+
+}
